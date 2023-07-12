efbcbe28673e2333bc3532532186e4241a65aefc
omranali2111 <aljabri1234@hotmail.com>
2023-07-12 10:38:59 +0400

    practice in List

---
+++ c-sharp-Lists/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_Lists
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public Person(int id, string name, string title)
+        {
+            Id = id;
+            Name = name;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}\nName: {Name}\nTitle: {Title}\n";
+        }
+    }
+}
---
+++ c-sharp-Lists/Program.cs
@@ -0,0 +1,39 @@
+﻿using c_sharp_Lists;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Remove(1);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+        list.Insert(0, 1);
+
+        foreach (int i in list)
+        {
+            Console.WriteLine(i);
+        }
+
+
+        List<Person> personlist= new List<Person>();
+        personlist.Add(new Person(1123, "omran", "Mr"));
+        personlist.Add(new Person(1125, "fatima", "Ms"));
+        personlist.Add(new Person(1129, "ali", "Mr"));
+
+
+
+        foreach (Person person in personlist)
+        {
+            Console.WriteLine(person.ToString());
+        }
+
+
+
+
+    }
+}
